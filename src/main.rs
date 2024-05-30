287940a99aade6635970c5d125a6e9b6adb8f7ce
newellg <newellg@users.noreply.github.com>
2024-05-30 23:54:20 +0100

    Final ver

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,19 @@ use std::{io::{self}};
 
 // Translation of Band Name Generator from Python
 
-fn say_band_name(place: &str, name: &str) {
-    println!("Your band name could be {} {}", place, name);
-}
+
 fn main() {
     let mut city = String::new();
     let mut pet :String = String::new();
     println!("Welcome to the Band Name Generator");
     println!("What city did you grow up in? : ");
     io::stdin().read_line(&mut city).expect("Read Line Failed.");
-    let trimmed_city = city.trim();
+    let city = city.trim();
     println!("What is the name of your pet? : ");
 
     io::stdin().read_line(&mut pet).expect("Read Line Failed.");
-    say_band_name(&trimmed_city, &pet );
+    println!("Your band name could be {} {}", city, pet);
 }
+
 
 
