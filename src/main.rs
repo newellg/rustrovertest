c6b8e10081d5a635620b06acf22b8052897cc2b1
newellg <newellg@users.noreply.github.com>
2024-05-30 23:48:14 +0100

    so much harder in rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use std::io;
+use std::{io::{self}};
 
-fn main() {
-    let mut name = String::new();
-    println!("What is your name? : ");
-    io::stdin().read_line(&mut name).expect("Read Line Failed.");
-    println!("Hello, {}", name);
+
+// Translation of Band Name Generator from Python
+
+fn say_band_name(place: &str, name: &str) {
+    println!("Your band name could be {} {}", place, name);
 }
+fn main() {
+    let mut city = String::new();
+    let mut pet :String = String::new();
+    println!("Welcome to the Band Name Generator");
+    println!("What city did you grow up in? : ");
+    io::stdin().read_line(&mut city).expect("Read Line Failed.");
+    let trimmed_city = city.trim();
+    println!("What is the name of your pet? : ");
+
+    io::stdin().read_line(&mut pet).expect("Read Line Failed.");
+    say_band_name(&trimmed_city, &pet );
+}
+
+
